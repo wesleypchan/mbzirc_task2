d6d0e413b72ab5501f47703953c9ad6b8ea3e18f
mizmizo <mizohana@jsk.imi.i.u-tokyo.ac.jp>
2017-03-14 19:03:56 +0900

    fix broken history

---
+++ mbzirc_task2_utils/scripts/start-remotepc-processing.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+PROGNAME=$(basename $0)
+LOCAL_IP="192.168.97.136"
+
+usage() {
+  echo "Usage: $PROGNAME [option]"
+  echo
+  echo "options:"
+  echo "  -h, --help"
+  echo "  -i, --local-ip <ARG>         specify remote ip for sending data. default ip is $LOCAL_IP"
+  echo "  -l, --large-data              send large data to remote pc. small data is used by default."
+  echo
+  exit 1
+}
+
+for OPT in "$@"
+do
+  case "$OPT" in
+    '-h'|'--help' )
+      usage
+      exit 1
+      ;;
+    '-i'|'--local-ip' )
+      if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        echo "$PROGNAME : -i and --local-ip option requires an argument!"
+      fi
+      LOCAL_IP="$2"
+      shift 1
+      ;;
+    '-l'|'--large-data' )
+      FLG_LARGE=1
+      shift 1
+      ;;
+    '--'|'-' )
+      shift 1
+      param+=( "$@" )
+      break
+      ;;
+    -*)
+      echo "$PROGNAME: unknown option $(echo $1 | sed 's/^-*//'). Check '$PROGNAME -h'" 1>&2
+      exit 1
+      ;;
+  esac
+done
+
+byobu-tmux start-server
+byobu-tmux new-session -s remote-pc -d -n roscore roscore \; new-window -n udp \; new-window -n joy \; new-window -n sixad
+sleep 1
+
+byobu-tmux send-keys -t roscore "roscore" C-m
+
+if [ -z $FLG_LARGE ]; then
+    byobu-tmux send-keys -t udp "roslaunch mbzirc_task2_network aero_remotepc.launch LOCAL_IP:='$LOCAL_IP' USE_LARGEDATA:='false'" C-m
+else
+    byobu-tmux send-keys -t udp "roslaunch mbzirc_task2_network aero_remotepc.launch LOCAL_IP:='$LOCAL_IP' USE_LARGEDATA:='true'" C-m
+fi
+
+byobu-tmux send-keys -t joy "rosrun joy joy_node" C-m
+byobu-tmux send-keys -t sixad "sixad -s" C-m
+
+
+byobu-tmux attach-session -t remote-pc
--- valve_task/scripts/start_roseus_aero_mbzirc_byobu.sh
+++ valve_task/scripts/start_roseus_aero_mbzirc_byobu.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 PROGNAME=$(basename $0)
+WRENCH_TARGET=4
 
 usage() {
   echo "Usage: $PROGNAME [option]"
   echo
   echo "options:"
   echo "  -h, --help"
+  echo "  -w, --wrench          option for set wrench target. set 3 for 19mm, 4 for 22mm"
   echo
   exit 1
 }
@@ -15,6 +17,13 @@ do
   case "$OPT" in
     '-h'|'--help' )
       usage
+      exit 1
+      ;;
+    '-w'|'--wrench' )
+      if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then                                                    
+        echo "$PROGNAME : -w and --wrench option requires an argument!"                           
+      fi
+      WRENCH_TARGET="$2"
       exit 1
       ;;
     '--'|'-' )
@@ -33,6 +42,7 @@ byobu-tmux start-server
 byobu-tmux new-session -s aero-mbzirc -d -n roscore \; new-window -n bringup \; new-window -n utils \; new-window -n roseus
 sleep 1
 
+# aero core system ======
 byobu-tmux send-keys -t roscore "roscore" C-m
 byobu-tmux send-keys -t bringup "/opt/ros/`rosversion -d`/bin/roslaunch aero_startup aero_bringup.launch" C-m
 sleep 7
@@ -40,5 +50,23 @@ byobu-tmux send-keys -t utils "/opt/ros/`rosversion -d`/bin/roslaunch aero_utils
 
 byobu-tmux send-keys -t roseus "rosnode kill /aero_servo_controller_service" C-m
 byobu-tmux send-keys -t roseus "rlwrap roseus `rospack find valve_task`/euslisp/start-roseus-aero-mbzirc.l" C-m
+# ====== aero core system
+
+# roseus nodes ======
+byobu-tmux new-window -n picking
+byobu-tmux send-keys -t picking "rlwrap roseus `rospack find mbzirc_task2_control`/euslisp/catch-wrench.l" C-m
+byobu-tmux send-keys -t picking "full-pick $WRENCH_TARGET :auto t :ready? t" # for rehearsal
+
+byobu-tmux new-window -n aligning
+byobu-tmux send-keys -t aligning "rlwrap roseus `rospack find mbzirc_task2_control`/euslisp/align-with-panel.l" C-m
+byobu-tmux send-keys -t aligning "align-with-panel" # for rehearsal
+byobu-tmux new-window -n approach
+byobu-tmux send-keys -t approach "rlwrap roseus `rospack find mbzirc_task2_control`/euslisp/approach-panel.l" C-m
+byobu-tmux send-keys -t approach "long-approach" # for rehearsal
+byobu-tmux new-window -n init-pos
+byobu-tmux send-keys -t init-pos "rlwrap roseus `rospack find mbzirc_task2_control`/euslisp/init-position.l" C-m
+byobu-tmux send-keys -t init-pos "go-to-init-position :init_x 40000 :init_y 20000" #for rehearsal
+
+# ====== roseus nodes
 
 byobu-tmux attach-session -t aero-mbzirc
